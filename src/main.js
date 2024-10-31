dd23001547373eca4ee53269887954bb94742be8
Ansh_sharma <keshu1234harshu@gmail.com>
2024-10-31 15:07:48 +0530

    rect

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-import { Application } from "pixi.js";
+import { Application, Graphics } from "pixi.js";
 
 (async()=>{
     const app = new Application();
-
+    
     await app.init({
         resizeTo:window,
-        backgroundColor: 0x15181C ,
+        backgroundColor: 0xfff000 ,
         antialias:true
     });
 
     app.canvas.style.position ='absolute';
+
+    const rectangle = new Graphics();
+
+    rectangle.rect(0,(innerHeight/2)+100,innerWidth,innerHeight/2)
+    .fill({
+        color:0x000000,
+        alpha:0.99
+    });
+
+    app.stage.addChild(rectangle);
 
     document.body.appendChild(app.canvas);
 })();
